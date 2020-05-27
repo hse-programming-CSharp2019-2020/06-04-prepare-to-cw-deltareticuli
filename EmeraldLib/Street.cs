9f62a9a67ec8f31f0002b11b9340ef59e23552c9
Ivan <deltareticuli@gmail.com>
2020-05-27 18:04:53 +0500

    Rider VCS test

--- EmeraldLib/Street.cs
+++ EmeraldLib/Street.cs
@@ -12,10 +12,8 @@ namespace EmeraldLib
     {
         public string Name { get; set; }
         public int[] Houses { get; set; }
-
         public static int operator ~(Street str) => str.Houses.Length;
         public static bool operator +(Street str) => str.Houses.Contains(7);
-
         public override string ToString()
         {
             return $"Name: {Name}, Houses: {string.Join(", ", Houses)}, Contains7: {+this}, Length: {~this}";
